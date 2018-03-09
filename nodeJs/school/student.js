374ec592c2d0bb1c28dc518297255a42dba87e9c
dengyt <2248545525@qq.com>
2018-03-09 16:05:33 +0800

    delete nodeJs dir

--- nodeJs/school/student.js
+++
@@ -1,5 +0,0 @@
-function add(student){
-    console.log('Add student:'+ student);
-}
-// 暴露方法
-exports.add = add;
--- nodeJs/school/teacher.js
+++
@@ -1,5 +0,0 @@
-function add(teacher){
-    console.log('Add teacher:'+ teacher);
-}
-// 暴露方法
-exports.add = add;
