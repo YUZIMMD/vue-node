ab2c1d988df6b24d95a6c231219a1ef690628354
dengyt <2248545525@qq.com>
2018-03-09 16:01:21 +0800

    delete node dir

--- node/myapp/db/sql_mapping.js
+++
@@ -1,7 +0,0 @@
-// sql语句
-var sql_mapping = {
-    user: {
-        query_user: 'select * from user'
-    }
-}
-module.exports.sql_mapping = sql_mapping;
--- node/myapp/routes/api.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var api = require('../api/user');
-
-/* GET users listing. /users路由*/
-router.get('/users', function(req, res, next) {
-    api.login(req, res);
-});
-module.exports = router;
